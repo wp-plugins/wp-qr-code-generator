9140d47092fc92628b961956f08790bec8886a7e
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-01 11:49:52 +0000

    Tested with wordpress version 4.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-qr-code-generator/tags/1.3@1235348 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,81 @@
+ === WP QR Code Generator ===
+Contributors: vivacityinfotech.jaipur
+Donate link:  http://bit.ly/1icl56K
+Tags: QR code , QR Code Wordpress Plugin , QR Code Generator , WP QR Code , QR Code widget , QR Code shortcodes , QR code in page , QR code in post ,  QR Code scanner . 
+Requires at least: 3.0
+Tested up to: 4.3
+License: GPLv2 or later
+
+An easy way to add your QR Code widget in your sidebars and add in your page .
+
+== Description ==
+
+(Quick Response code) A two-dimensional bar code that is widely used to cause a Web page to download into the user's smartphone when scanned with a mobile tagging app. QR Code widget and QR Code shortcodes generate by WP QR Code Generator plugin . Its easy to use in your sidebars and  shortcode [vqr/] for pages or post.
+
+
+= Features =
+
+    -Easy install
+    -Very easy to configure.
+    
+    
+
+= Rate Us / Feedback =
+
+Please take the time to let us and others know about your experiences by leaving a review, so that we can improve the plugin for you and other users.
+
+= Want More? =
+
+If You Want more functionality or some modifications, just drop us a line what you want and We will try to add or modify the plugin functions.
+
+If you like the plugin please [Donate here](http://tinyurl.com/owxtkmt). 
+
+== Available Languages ==
+
+    English
+
+== Installation ==
+
+Installation consists of following steps:
+
+1. Upload "WP QR Code Generator" to the /wp-content/plugins/ directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the "WP Google Analytics Scripts" QR Code widget in your widget area and a icon at you pages or post for shotcodes.
+
+== Frequently Asked Questions ==
+
+= Will my QR codes "expire"? =
+
+No. The QR codes (and any underlying short URL's) created by both free users and paid subscribers are permanent and will continue to function indefinitely, however temporary scan limits may apply as outlined below.
+
+= Can I use your QR codes commercially? =
+
+Yes. There are no restrictions on commercial use however temporary scan limits may apply as outlined below.
+
+= Are your QR codes ad-free? =
+
+Yes. Our QR codes are guaranteed 100% ad-free. If you see an ad after scanning one of our QR codes, your scanning app put it there.
+
+= Is a license required to create QR codes? =
+
+No. The QR code is clearly defined and published as an open ISO standard. Denso Wave owns the patent rights on the QR code creation process, and the methods used to encode and create them, but has chosen not to exercise those rights. The term "QR code" itself is a registered trademark of Denso Wave Incorporated.
+
+= Do QR Codes Have To Be A Certain Size? =
+
+People often ask how big or small (physically) a QR Code can be, well the simple answer is that there are no limits, a QR Code doesn’t have to be a set size and can be printed to any required size. The surface area of a QR Code is only limited by its environment and expected use combined with the resolution of the scanning device, there isn’t a technical limitation.
+
+
+== Screenshots ==
+1.  WP QR Code Generator plugin appears after activation in Widget section  .
+2.  WP QR Code Generator plugin appears after activation in Pages or post section.
+
+== Changelog ==
+= 1.3 =
+* Make compatible with the latest version for wordpress.
+
+= 1.2 =
+* Added the feature to change Background color.
+
+= 1.0 =
+* Initial release
+
---
+++ wp-qr-code-generator.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Plugin Name: WP QR Code Generator
+Plugin URI: http://www.vivacityinfotech.net
+Description: An easy way to add your QR Code widget in your sidebars and add in your page .
+Version: 1.3
+Author URI: http://www.vivacityinfotech.net
+* Author Email: support@vivacityinfotech.net
+Requires at least: 3.8
+Text Domain: WP-QR-Code-Generator
+License: vivacityinfotech
+*/
+/*
+Copyright 2014  Vivacity InfoTech Pvt. Ltd.  (email : support@vivacityinfotech.net)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+add_filter('plugin_row_meta', 'RegisterPluginLinks_qr',10, 2);
+function RegisterPluginLinks_qr($links, $file) {
+	if ( strpos( $file, 'wp-qr-code-generator.php' ) !== false ) {
+		$links[] = '<a href="https://wordpress.org/plugins/wp-qr-code-generator/faq/">FAQ</a>';
+		$links[] = '<a href="mailto:support@vivacityinfotech.com">Support</a>';
+		$links[] = '<a href="http://bit.ly/1icl56K">Donate</a>';
+	}
+	return $links;
+}
+
+
+
+add_action('admin_enqueue_scripts','admin_jquery_link');
+add_action('wp_enqueue_scripts','front_jquery_link');
+add_shortcode('vqr','qr_shortcode');
+
+function admin_jquery_link($hook_suffix){
+ wp_enqueue_style( 'wp-color-picker' );
+ wp_enqueue_script( 'admin_script', plugins_url('/js/admin_script.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+}
+ 
+function front_jquery_link(){
+wp_enqueue_script( 'WP-QR-Code-Generator-js', plugins_url( 'qrcode.js' , __FILE__ ));
+}
+
+function qr_shortcode($attr,$cont=null){
+extract(shortcode_atts( array(
+		'msg' => " WP QR Code Generator",
+		'size' => 160,
+                'color_Light' => "'.$color_Light.'",
+		'level' => "Q"
+	 	), $attr ));
+		$r=rand(0,9999);
+return '<div id="vqr'.$r.'"></div>
+<script type="text/javascript">
+new QRCode(document.getElementById("vqr'.$r.'"), {
+	text: "'.$msg.'",
+	width: '.$size.',
+	height: '.$size.',
+	colorDark : "#222222",
+	colorLight : "'.$color_Light.'",
+	correctLevel : QRCode.CorrectLevel.'.$level.'
+});
+</script>';
+}
+add_action( 'init', 'qrcode_submit' );
+function qrcode_submit() {
+    add_filter( "mce_external_plugins", "qr_getvalue" );
+    add_filter( 'mce_buttons', 'registration_qr' );
+}
+function qr_getvalue( $plugin_array ) {
+    $plugin_array['vqr'] = plugins_url( 'js/WP-QR-Code-Generator.js' , __FILE__ );
+    return $plugin_array;
+}
+function registration_qr( $buttons ) {
+    array_push( $buttons,'qrcode_submitvalue' );
+    return $buttons;
+}
+
+class qrcode_Widget extends WP_Widget {
+	
+	public function __construct() {
+		parent::__construct(
+	 		'qrcode_Widget', 
+			'WP QRcode Widget', 
+			array( 'description' =>  ' QRcode Generator widget' )
+		);
+	}
+
+	public function widget( $args, $input_value ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $input_value['title'] );
+		$text = $input_value[ 'text' ];
+		$size = $input_value[ 'size' ];
+                $color_Light = $input_value[ 'color_Light' ];
+		$level = $input_value[ 'level' ];
+		echo $before_widget;
+		if ( ! empty( $title ) )
+			echo $before_title . $title . $after_title;
+		echo '<div id="getvalue"></div>
+<script type="text/javascript">
+new QRCode(document.getElementById("getvalue"), {
+	text: "'.$text.'",
+	width: '.$size.',
+	height: '.$size.',
+	colorDark : "#222222",
+	colorLight : "'.$color_Light.'",
+	correctLevel : QRCode.CorrectLevel.'.$level.'
+});
+
+
+</script>';
+
+		echo $after_widget;
+	}
+
+ 	public function form( $input_value ) {
+		$text = isset($input_value[ 'text' ])?$input_value[ 'text' ]:"Enter TEXT";
+		$size = isset($input_value[ 'size' ])?$input_value[ 'size' ]:"150";
+		$title = isset($input_value[ 'title' ])?$input_value[ 'title' ]:" QR Code Generator";
+                $color_Light = isset($input_value[ 'color_Light' ])?$input_value[ 'color_Light' ]:"#c1c1c1";
+		$level = $input_value[ 'level' ];
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>">TITLE:</label> <br/>
+		<input class="block_text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /><br/>
+		<label for="<?php echo $this->get_field_id( 'text' ); ?>">TEXT:</label> <br/>
+		<textarea class="block_text" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>" ><?php echo esc_attr( $text ); ?></textarea><br/>
+		<label for="<?php echo $this->get_field_id( 'size' ); ?>">SIZE:</label> <br/>
+		<input class="block_text" id="<?php echo $this->get_field_id( 'size' ); ?>" name="<?php echo $this->get_field_name( 'size' ); ?>" type="text" value="<?php echo esc_attr( $size ); ?>" /><br/>
+                <label for="<?php echo $this->get_field_id( 'color_Light' ); ?>">BACKGROUND COLOR:</label> <br/>
+		<input class="color_Light intentColor" id="<?php echo $this->get_field_id( 'color_Light' ); ?>" name="<?php echo $this->get_field_name( 'color_Light' ); ?>" type="text" value="<?php echo esc_attr( $color_Light ); ?>" data-default-color="<?php echo esc_attr( $color_Light ); ?>"/><br/>
+		<label for="<?php echo $this->get_field_id( 'level' ); ?>">LEVEL:</label> <br/>
+		<select class="block_text" id="<?php echo $this->get_field_id( 'level' ); ?>" name="<?php echo $this->get_field_name( 'level' ); ?>">
+	<option value="L" <?php if($level=='L' ) echo 'selected'; ?> >L</option>
+	<option value="M" <?php if($level=='M') echo 'selected'; ?>>M</option>
+	<option value="H" <?php if($level=='H' ) echo 'selected'; ?>>H</option>
+	<option value="Q" <?php if($level=='Q' || $level == ''  ) echo 'selected'; ?>>Q</option>
+	
+	</select>
+		</p>
+		<?php 
+	}
+
+	public function update( $new_inputvalue, $old_inputvalue ) {
+		$input_value = array();
+		$input_value['title'] = strip_tags( $new_inputvalue['title'] );
+		$input_value['text'] = strip_tags( $new_inputvalue['text'] );
+		$input_value['size'] = strip_tags( $new_inputvalue['size'] );
+                $input_value['color_Light'] = strip_tags( $new_inputvalue['color_Light'] );
+		$input_value['level'] = strip_tags( $new_inputvalue['level'] );
+		return $input_value;
+	}
+
+}
+add_action( 'widgets_init', create_function( '', 'register_widget( "qrcode_Widget" );' ) );
+
+?>
